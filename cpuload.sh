bac4c4b539274890ec26978fdc84e1d1268720c2
suhasinish05 <hasinish6@gmail.com>
2025-04-25 15:46:09 +0530

    Update cpuload.sh

--- cpuload.sh
+++ cpuload.sh
@@ -4,6 +4,6 @@
 load=$(uptime | awk -F'load average:' '{ print $2 }' | cut -d',' -f1 | sed 's/ //g')
 threshold=5.0
 if (( $(echo "$load > $threshold" | bc -l) )); then
-  echo "High CPU Load Detected: $load" | mail -s "CPU Alert" hasinish05@gmail.com
+  echo "High CPU Load Detected: $load" | mail -s "CPU Alert" hasinish6@gmail.com
 fi
 
