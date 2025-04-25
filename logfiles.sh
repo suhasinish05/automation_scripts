0a333c1c799696cb8ecff82f570b8ae92a21fde3
suhasinish05 <hasinish6@gmail.com>
2025-04-25 10:37:15 +0530

    Create logfiles.sh

---
+++ logfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# script for Clean Old Log Files Automatically / Auto-Delete Log Files Older Than 7 Days
+
+# for more then 7 days , change 1 =7
+
+find /home/ec2-user -name "*.log" -type f -mtime -1 -delete
+
+echo "deleted log files older than 1 day,to  ensure our disk doesn’t fill up."  | mail -s " Auto-Deleted Log Files Older Than 1 Day" hasinish05@gmail.com
