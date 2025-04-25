4d01fca7cb54f44b7eda3eff1dd3040375169e6f
suhasinish05 <hasinish6@gmail.com>
2025-04-25 10:47:15 +0530

    Create autobackup.sh

---
+++ autobackup.sh
@@ -0,0 +1,18 @@
+#Backup Important Project Folder / Auto Backup Project Folder Daily 
+#!/bin/bash
+# Backup the 'functions' directory with current date
+
+
+src="/home/ec2-user/functions"
+dest="/home/ec2-user/Backups"
+timestamp=$(date +%F)
+backup_file="$dest/mydata_backup_$timestamp"
+
+mkdir -p "$dest"
+
+
+cp -r $src $backup_file
+
+
+echo "Backup completed successfully." | mail -s "Backup Success" hasinish6@gmail.com
+~
